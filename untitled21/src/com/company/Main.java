3242f6053d28f5c9e40f2e3f5892f35dcdf34e62
ogabek2606 <110012533+ogabek2606@users.noreply.github.com>
2022-07-30 03:36:43 -0400

    Add files via upload

---
+++ untitled21/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+            int a = 5, b = 10, c = 15;
+
+            int sum = a + b + c;
+            int average = sum / 3;
+
+            System.out.println (average);
+
+        }
+}
